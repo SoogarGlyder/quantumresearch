247afb7fde661802f946b9e9187685413bb41bdf
SoogarGlyder <p7t7kjwxrb@privaterelay.appleid.com>
2026-07-13 15:28:06 +0700

    Change response check from 'ok' to 'sukses'

--- components/admin/ModalKuis.jsx
+++ components/admin/ModalKuis.jsx
@@ -297,7 +297,7 @@ export default function ModalKuis({ isOpen, onClose, jadwal, kuisLama, adminId, 
     setShowImportPanel(!showImportPanel);
     if (!showImportPanel && listRiwayatKuis.length === 0) {
       const res = await getRiwayatKuisPengajar(adminId);
-      if (res.ok) setListRiwayatKuis(res.data);
+      if (res.sukses) setListRiwayatKuis(res.data);
     }
   };
 
@@ -352,7 +352,7 @@ export default function ModalKuis({ isOpen, onClose, jadwal, kuisLama, adminId, 
         res = await simpanKuis(jadwal._id, adminId, formSoalSiapSimpan, durasiUjian); // Gunakan data hasil konversi
       }
 
-      if (res.ok) {
+      if (res.sukses) {
         alert(isModeBankSoal ? "✅ Master Soal Berhasil Disimpan di Bank!" : "✅ Kuis CBT Berhasil Dipublikasikan!");
         if (muatData) muatData(); 
         onClose();
